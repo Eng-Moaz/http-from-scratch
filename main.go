cdd98be32847a6802fcd3a11958250f7b3172c96
Eng-Moaz <156330236+Eng-Moaz@users.noreply.github.com.>
2026-05-25 06:05:43 +0300

    8 bytes file reading

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+	"log"
+	"io"
+	"os"
+)
 
 func main(){
-	fmt.Println("Just starting the project :)")
+	file_name := "messages.txt"
+	file, err := os.Open(file_name)
+	if err != nil{
+		log.Fatalf("couldn't open %s: Error %s",file_name, err)
+	}
+	defer file.Close()
+
+	for {
+		b := make([]byte,8,8)
+		_, err := file.Read(b)
+		if err != nil{
+			if errors.Is(err, io.EOF){
+				break
+			}
+			fmt.Printf("error: %s\n",err)
+			break
+		}
+		printed := string(b[:])
+		fmt.Printf("read: %s\n", printed)
+	}
 }
